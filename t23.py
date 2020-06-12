4b04a00342ba777b5bf95593ee802dc78c22c866
unknown <heraldjose10@gmail.com>
2020-06-12 12:30:37 +0530

    added mongodb

--- t23.py
+++ t23.py
@@ -1,14 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May 24 10:45:02 2020
-
-@author: herald jose
-"""
-
 from flask import Flask
 from flask import render_template
 from flask import request
 import requests
+from datetime import datetime
+from pymongo import MongoClient
 
 
 app = Flask(__name__)
@@ -16,6 +11,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    client=MongoClient("mongodb://heraldjose10:9400840572@covid-shard-00-00-9pl1n.mongodb.net:27017,covid-shard-00-01-9pl1n.mongodb.net:27017,covid-shard-00-02-9pl1n.mongodb.net:27017/covid?ssl=true&replicaSet=Covid-shard-0&authSource=admin&retryWrites=true&w=majority")
+    db=client.covid
+    data1,d2=get_data()
+    update_data(db,data1)
+    cumulative_update(db,d2)
     data=directdata()
     drop=dropdownmenu(data)
     return render_template('herald_test v1.html', states=sorted(drop.keys()))
@@ -63,6 +63,47 @@ def directdata():
     return(r.json().get('statewise'))
 
 
+def get_data():
+    url='https://api.covid19india.org/data.json'
+    r=requests.get(url)
+    data1=r.json().get('statewise')
+    dat2=r.json().get('cases_time_series')
+    return(data1,dat2)
 
 
-                
+def update_data(db,list1):
+    for i in list1:
+                inputs={
+                'name':i.get('state'),
+                'data':[
+                    {'active':i.get('active'),
+                    'total':i.get('confirmed'),
+                    'deaths':i.get('deaths'),
+                    'recoveries':i.get('recovered'),}
+                ],
+                'date':datetime.utcnow()
+                }
+                db.covid_india_test.replace_one({'name':i.get('state')},inputs)
+
+
+def cumulative_update(db,list1):
+    li=list1[-3:]
+    
+    for i in li:
+        lis=(i.get('date')).split(' ')
+        months=['herald','January','February','March','April','May','June','July']
+        dt=str(months.index(lis[1]))+'-'+str(lis[0])
+        input1={
+                'daily_conf':i.get('dailyconfirmed'),
+                'daily_xed':i.get('dailydeceased'),
+                'daily_rec':i.get('dailyrecovered'),
+                'cum_conf':i.get('totalconfirmed'),
+                'cum_xed':i.get('totaldeceased'),
+                'cum_rec':i.get('totalrecovered'),
+                'date':dt
+                }
+        
+        if (bool(db.cumulative.find_one({'date': dt})))==True:
+            pass
+        else:
+            db.cumulative.insert_one(input1)
